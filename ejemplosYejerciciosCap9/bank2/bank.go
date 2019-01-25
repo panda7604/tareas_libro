2a2ba4b2015ad430e1dfdcbdc5af9bcee9d259ff
panda7604 <lospandax2017@gmail.com>
2019-01-24 22:17:19 -0500

    magia

---
+++ ejemplosYejerciciosCap9/bank2/bank.go
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+package bank
+
+
+var (
+	sema    = make(chan struct{}, 1) 
+	balance int
+)
+
+func Deposit(amount int) {
+	sema <- struct{}{} 
+	balance = balance + amount
+	<-sema 
+}
+
+func Balance() int {
+	sema <- struct{}{} 
+	b := balance
+	<-sema 
+	return b
+}
+
+
