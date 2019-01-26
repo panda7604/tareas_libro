3575f030a412ac246264186d5eac69df786c2b34
panda7604 <lospandax2017@gmail.com>
2019-01-26 09:48:27 -0500

    magia

---
+++ ejemplosYejerciciosCap13/bzipper/main.go
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"gopl.io/ch13/bzip"
+)
+
+func main() {
+	w := bzip.NewWriter(os.Stdout)
+	if _, err := io.Copy(w, os.Stdin); err != nil {
+		log.Fatalf("bzipper: %v\n", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Fatalf("bzipper: close: %v\n", err)
+	}
+}
+
+
